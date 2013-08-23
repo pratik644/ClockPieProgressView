24d5f0abecbf2049fa06a3e390a23e8f6e62f99c
sk <vs@Macbook-Pro.(none)>
2013-08-23 09:48:43 +0530

    Initial Commit

---
+++ SKSinglePie/SKViewController.m
@@ -0,0 +1,34 @@
+//
+//  SKViewController.m
+//  SKSinglePie
+//
+//  Created by Nik on 23/08/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "SKViewController.h"
+
+@interface SKViewController ()
+
+@end
+
+@implementation SKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ SKSinglePie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SKSinglePie
+//
+//  Created by Nik on 23/08/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKAppDelegate class]));
+    }
+}
